c843e96c02aefa6f91f9978d5462aaf6a68fae3b
Stefano Chang <schang@godaddy.com>
2017-03-11 16:12:17 -0700

    Add db functions

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -2,19 +2,58 @@ import {Component, Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
+import {DynamoDB} from 'aws-sdk';
 
 @Injectable()
-@Component({
-})
+@Component({})
 export class GitHubService {
-	url = 'MONGO_DB_URL';
+  url = 'https://um7sfkonal.execute-api.us-west-2.amazonaws.com/hack';
+  tableName = 'swhack';
+  dynamo = new DynamoDB();
 
-	constructor(private http: Http) {
-            console.log("MongoDB connection is functioning...");
-    }
+  constructor(private http: Http) {
+    console.log("Dynamo connection is functioning...");
+  }
 
-    fullQuery(){
-		return this.http.get(this.url).map(res => res.json());
-	}
+  select(id: string) {
+    let dynamoQueryObj = {
+      "TableName": this.tableName,
+      "Key": {
+        "id": {
+          "S": id
+        }
+      }
+    };
+    this.dynamo.getItem(dynamoQueryObj, (err, data) => {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        console.log("Success: " + data);
+      }
+    });
+  }
+
+  insert(obj: any) {
+    let dynamoInsertObj = {
+      "TableName": this.tableName,
+      "Item": {}
+    };
+
+    Object.keys(obj).forEach((key) => {
+      if (key === 'username') {
+        dynamoInsertObj.Item["id"] = {"S": obj[key]};
+      } else {
+        dynamoInsertObj.Item[key] = {"S": obj[key]};
+      }
+    });
+
+    this.dynamo.putItem(dynamoInsertObj, (err, data) => {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log('Success: ' + data);
+      }
+    });
+  }
 
 }
