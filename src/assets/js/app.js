44f08b2dd8235a7b8863780da787ae912ad7cb8b
Stefano Chang <schang@godaddy.com>
2017-03-12 04:18:27 -0700

    update

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,3 @@
-var hack = function () {
-  function print() {
-    var t = $('#jquery-applicantList');
-    console.log(t.val());
-  }
-
-  return {
-    print: print
-  }
-}();
+(function () {
+  
+})();
