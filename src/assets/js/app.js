f1200fb532fc9185c70ddf9e7286044d6eea1ec8
Stefano Chang <schang@godaddy.com>
2017-03-12 04:24:24 -0700

    add jquery code

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,15 @@
 (function () {
-  
+  var applicantData;
+
+  function set(newData) {
+    applicantData = newData;
+  }
+
+  function print() {
+    console.log(applicantData);
+  }
+
+  return {
+    print: print
+  }
 })();
