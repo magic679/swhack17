4faf211d1492ad27f7ddbf1bc59cdb003fda04f5
Jared Rounsavall <jaredrounsavall@macbook-pro-2.mobile.asu.edu>
2017-03-12 04:03:05 -0700

    Hacked fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,12 @@
+(function(){
+  setTimeout(function(){
+    document.getElementById('someid').addEventListener('click', function(){
+      var newgraph = recruiterGraphs.initialize('.graph-content');
+      newgraph.scatterData(window.doingit, 'created_at', 'payload.size');
+    });
+  }, 5000);
+})();
+
 var hack = function(){
     console.log('in jquery');
     console.log(applicantList);
